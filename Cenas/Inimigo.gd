0e00c78edaa542c0e04fe6a22fda66cebc105c83
ALUNO K9 <alunok9@acad.unifor.br>
2023-09-25 10:29:49 -0300

    portalkkk

---
+++ Cenas/Inimigo.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+var direcao = 1
+var speed = 50
+func _physics_process(delta):
+	translate(Vector2(speed*direcao*delta, 0))
+
+func _on_timerPatrulha_timeout():
+	direcao*=-1
+ 
+
+func _on_areaImpacto_body_entered(body):
+	queue_free()
