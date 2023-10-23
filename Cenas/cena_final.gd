61e302f1cf7d46dd57bca45d234acc05a8c4bcf8
Erich <erichs890@gmail.com>
2023-10-23 09:01:37 -0300

    TERMINADO

--- Cenas/cena_final.gd
+++ Cenas/cena_final.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-
-
-
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://Cenas/menu.tscn")
 func _on_Button_pressed():
+	Global.reset()
 	get_tree().change_scene("res://Cenas/menu.tscn")
--- global.gd
+++ global.gd
@@ -4,3 +4,8 @@ extends Node
 var score = 0
 var vidas = 3
 var level = 1
+
+func reset():
+	score = 0
+	vidas = 3
+	level = 1
